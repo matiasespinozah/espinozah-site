46bc658551ca4f93b8bc344d62d4fa17559c37dd
Matías Espinoza <matias.espinoza@tinet.cl>
2021-12-11 20:01:05 -0300

    add: se agrego angular analitics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,44 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
+// enviroments
+import { environment } from '@env/environment';
+
+// rxjs
+import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
+
+// gtag from google analytics
+declare var gtag: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  /**
+   * constructor
+   *
+   * @param router router de angular
+   */
+  constructor(private router: Router) {
+    this.googleAnalitycs();
+  }
+
+  /**
+   * escucha los cambios de google analitics y los trasmite a google
+   */
+  private googleAnalitycs(): void {
+    if (environment.production) {
+      (
+        this.router.events.pipe(
+          filter(event => event instanceof NavigationEnd)
+        ) as Observable<NavigationEnd>
+      ).subscribe((event: NavigationEnd) => {
+        gtag('config', 'G-GQ9713VGKL', {
+          page_path: event['urlAfterRedirects']
+        });
+      });
+    }
+  }
+}
